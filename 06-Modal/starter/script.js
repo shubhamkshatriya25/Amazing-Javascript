96551e4e138656671036decdc7325f77de3692f9
shubhamkshatriya25 <shubhamkshatriya521@gmail.com>
2021-02-09 18:26:55 +0530

    06-Modal Completed

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+let showModal = document.querySelectorAll('.show-modal');
+let close = document.querySelector('.close-modal');
+let modal = document.querySelector('.modal');
+let overlay = document.querySelector('.overlay');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+};
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', openModal);
+}
+
+close.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
